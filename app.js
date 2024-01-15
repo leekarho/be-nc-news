25549b00597877b506303e36d7ecb649b443ad31
karho.lee <k-h_lee@hotmail.co.uk>
2024-01-15 16:22:30 +0000

    created and added /api/articles

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const { getAllTopics } = require("./controllers/topics.controller");
 const { getAllEndpoints } = require("./controllers/endpoints.controller");
-const { getArticleById } = require("./controllers/articles.controller");
+const {
+  getArticleById,
+  getAllArticles,
+} = require("./controllers/articles.controller");
 const app = express();
 
 app.use(express.json());
@@ -11,6 +14,8 @@ app.get("/api", getAllEndpoints);
 app.get("/api/topics", getAllTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getAllArticles);
 
 app.use((err, req, res, next) => {
   if (err.msg === "Not found") {
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,9 +1,23 @@
-const { selectArticleById } = require("../models/articles.model");
+const {
+  selectArticleById,
+  selectAllArticles,
+} = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
   const id = req.params;
   selectArticleById(id)
     .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getAllArticles = (req, res, next) => {
+  selectAllArticles()
+    .then((article) => {
+      console.log(article, "<--article");
       res.status(200).send({ article });
     })
     .catch((err) => {
